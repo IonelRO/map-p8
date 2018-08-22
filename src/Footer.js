272acfcc92487a2f258f26795fc9c5feda6d5cc5
ionel.dobre <dobre_ionel@yahoo.com>
2018-08-22 16:27:52 +0300

    Final makeup

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+
+import React, {Component} from 'react';
+
+
+class Footer extends Component {
+
+render() {
+
+return(
+      <footer tabIndex={0}>
+          		 Project by Dobre Ionel for the Udacity FEND Nanodegree. Integrated with
+               <a href="https://developers.google.com/maps/" tabIndex={0} aria-label=" Google maps for developer site "> Google Maps </a>
+               and <a href="https://developer.foursquare.com/" tabIndex={0} aria-label=" Foursquare for developer site ">Foursquare</a> APIs.
+               
+      	</footer>
+);
+}
+}
+export default Footer;
--- src/maps.js
+++ src/maps.js
@@ -4,10 +4,7 @@ let token = localStorage.token
 if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8)
 
-const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
-}
+
 function handleErrors(response) {
     if (!response.ok) {
         throw Error(response.statusText);
@@ -28,23 +25,8 @@ export const getSelectedAll = (query) =>
     .then(data => data.response.venues)
    .catch(error => {console.log('Error While getting All Locations data from FourSquare API', error)})
 
-export const getflickrImg = () =>
-  fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=cc486d5a638ecbda69e566971f130c71&tags=&lat=45.0411633&lon=23.2662036&format=json&nojsoncallback=1&auth_token=72157698813831471-52ba3a104b4bb378&api_sig=99f32933549a1428785e990e613691be')
-   .then(handleErrors)
-   .then(res => res.json())
-   .then(data => data.response.photos.photo)
-   .catch(error => {console.log('Error While getting All Locations data from flickr API', error)})
 
-export const getVenueDetails = (venueId)=> {
-let venueDetailsUrl =[`/venues/${venueId}?`,
-					  `client_id=HEZXEFLMPE4HONPDQEGOSWEUYNSAIKUZKXRBNPZSK55QK4PC`,
-					  `&client_secret=E0TQXTI1GT4BRRABITQIQZSPYSFSBJ0UHRQZH5U00X30DP5B&v=20180820`].join("")
 
-return	fetch(`${api}${venueDetailsUrl}`)
-		.then(res => res.json())
-		.then(data => data.response.venue)
-		.catch(error => {console.log(`Error while Getting Venue Details `, error)})
-}
 
 
 
